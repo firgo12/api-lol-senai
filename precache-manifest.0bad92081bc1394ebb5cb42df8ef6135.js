1793b328b729504e01fabbb55ee1642c159e5e4c
firgo12 <60897048+firgo12@users.noreply.github.com>
2020-09-03 17:57:23 -0300

    Mudei

---
+++ precache-manifest.0bad92081bc1394ebb5cb42df8ef6135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61109287cd1c8ec8141642cb24f98ab7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "385c2baf4b095b1c2bc1",
+    "url": "./static/js/2.665cca93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.665cca93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b52827e5e64a54fc967",
+    "url": "./static/js/main.0b560e74.chunk.js"
+  },
+  {
+    "revision": "b6850830b7cf179e9d10",
+    "url": "./static/js/runtime-main.3354df24.js"
+  }
+]);
